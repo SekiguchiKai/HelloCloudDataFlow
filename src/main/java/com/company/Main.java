836e2a91ba41a1847ba8061bbc4fd56b4d3de1a0
sekky0905 <ks.sekiguchi.0905@gmail.com>
2017-07-11 21:58:57 +0900

    動くようになった

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -4,25 +4,56 @@ import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.PCollection;
+
 
 /**
  * メイン
  * Created by sekiguchikai on 2017/07/05.
  */
 public class Main {
+    // DoFnを実装したクラス
+    // DoFnの横の<T,T>でinputとoutputの方の定義を行う
+    // この場合、StringがinputでIntegerがoutput
+    static class FilterEvenFn extends DoFn<String, String> {
+        // 実際の処理ロジックにはこのアノテーションをつける
+        @ProcessElement
+        // ProcessContextは、inputを表すobject
+        // 自分で定義しなくてもBeam SDKが勝手に取ってきてくれる
+        public void processElement(ProcessContext c) {
+            System.out.print(c.element());
+            int num = Integer.parseInt(c.element());
+            if (num % 2 == 0) {
+                System.out.println("ifの結果" + num);
+                c.output(String.valueOf(num));
+            }
+        }
+    }
+
+    // インプットデータのパス
+    private static final String INPUT_FILE_PATH = "./dataflow_number_test.csv";
+    // アウトデータのパス
+    private static final String OUTPUT_FILE_PATH = "./sample.csv";
+
     public static void main(String[] args) {
-        System.out.println("テスト");
-        // Pipelineのoptionを定義
-        // PipelineOptionsFactory.create : Creates and returns an object that implements {@link PipelineOptions}.
-        PipelineOptions options = PipelineOptionsFactory.fromArgs(args).create();
+        // まずPipelineに設定するOptionを作成する
+        // 今回は、ローカルで起動するため、DirectRunnerを指定する
+        // ローカルモードでは、DirectRunnerがすでにデフォルトになっているため、ランナーを設定する必要はない
+        PipelineOptions options = PipelineOptionsFactory.create();
 
-        // Pipelineクラスのインスタンスを生成
-        Pipeline pipeline = Pipeline.create();
+        // Optionを元にPipelineを生成する
+        Pipeline pipeline = Pipeline.create(options);
 
-        // PCollectionを作成
-        PCollection<String> lines = pipeline.apply(
-                "ReadMyFile", TextIO.read().from("protocol://path/to/some/inputData.txt"));
+        // inout dataを読み込んで、そこからPCollection(パイプライン内の一連のデータ)を作成する
+        PCollection<String> stringData = pipeline.apply(TextIO.read().from(INPUT_FILE_PATH));
 
+        // 処理
+        PCollection<String> even = stringData.apply(ParDo.of(new FilterEvenFn()));
+        // 書き込む
+        even.apply(TextIO.write().to(OUTPUT_FILE_PATH));
+
+        pipeline.run().waitUntilFinish();
     }
 }
